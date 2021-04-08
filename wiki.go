191ad7b52bdbda7d6339830513308c4875e1eac6
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2021-04-08 21:26:01 +0000

    Add edit functionality and refactor code in main

--- wiki.go
+++ wiki.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"html/template"
 )
 
 type Page struct {
@@ -28,13 +28,39 @@ func loadPage(title string) (*Page,error) {
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080",nil))
 }
 
 func viewHandler (w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
 }
 
+func editHandler (w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
 
+func renderTemplate (w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w,p)
+}
+
+func saveHandler (w http.ResponseWriter, r *http.Reqeust) {
+	title := r.URL.Path[len("/save/":]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w,r, "/view/"+title, http.StatusFound
+}
