80cc4722cd23d4f859fe3540247283725fd5e923
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2021-04-09 10:44:21 +0000

    Basic webserver with view, create, edit page functions complete

--- wiki.go
+++ wiki.go
@@ -31,17 +31,13 @@ func loadPage(title string) (*Page,error) {
 }
 
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("save/", saveHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 	log.Fatal(http.ListenAndServe(":8080",nil))
 }
 
-func viewHandler (w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w,r)
-	if err != nil {
-	  return
-	}
+func viewHandler (w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -50,8 +46,7 @@ func viewHandler (w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "view", p)
 }
 
-func editHandler (w http.ResponseWriter, r *http.Request) {
-	title,err := getTitle(w,r)
+func editHandler (w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -66,8 +61,7 @@ func renderTemplate (w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
-func saveHandler (w http.ResponseWriter, r *http.Reqeust) {
-	title, err := getTitle(w,r)
+func saveHandler (w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
@@ -75,14 +69,26 @@ func saveHandler (w http.ResponseWriter, r *http.Reqeust) {
 	  http.Error(w, err.Error(), http.StatusInternalServerError)
 	  return
 	}
-	http.Redirect(w,r, "/view/"+title, http.StatusFound
+	http.Redirect(w,r, "/view/"+title, http.StatusFound)
 }
 
-func getTitle (w http.ResponseWriter, r *http.Request) (string, error) {
+/* func getTitle (w http.ResponseWriter, r *http.Request) (string, error) {
 	m := validPath.FindStringSubmatch(r.URL.Path)
 	if m == nil {
 	  http.NotFound(w,r)
 	  return "", errors.New("invalid Page Title")
 	}
 	return m[2], nil //The title is the second subexpression
+}
+*/
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	    return func(w http.ResponseWriter, r *http.Request) {
+		    m := validPath.FindStringSubmatch(r.URL.Path)
+		    if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
 }
